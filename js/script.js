619cb69fa8c5a029b540fb95f9bc8cccb16a2c02
sengminyun <64954108+sengminyun@users.noreply.github.com>
2020-07-04 15:27:33 +0900

    a

--- js/script.js
+++ js/script.js
@@ -97,3 +97,64 @@ $(function(){
     });
 
 });
+
+
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+
+    });
+});
+
+$(function(){
+    $('.right-img1').mouseover(function(){
+       $('.img1-sub').css('display','block');
+    });
+    $('.right-img1').mouseleave(function(){
+        $('.img1-sub').css('display','none');
+     });
+     $('.right-img2').mouseover(function(){
+        $('.img2-sub').css('display','block');
+     });
+     $('.right-img2').mouseleave(function(){
+         $('.img2-sub').css('display','none');
+      });
+      $('.right-img3').mouseover(function(){
+        $('.img3-sub').css('display','block');
+     });
+     $('.right-img3').mouseleave(function(){
+         $('.img3-sub').css('display','none');
+      });
+      $('.right-img8').mouseover(function(){
+        $('.img8-sub').css('display','block');
+     });
+     $('.right-img8').mouseleave(function(){
+         $('.img8-sub').css('display','none');
+      });
+      $('.right-img5').mouseover(function(){
+        $('.img5-sub').css('display','block');
+     });
+     $('.right-img5').mouseleave(function(){
+         $('.img5-sub').css('display','none');
+      });
+      $('.right-img6').mouseover(function(){
+        $('.img6-sub').css('display','block');
+     });
+     $('.right-img6').mouseleave(function(){
+         $('.img6-sub').css('display','none');
+      });
+      $('.right-img7').mouseover(function(){
+        $('.img7-sub').css('display','block');
+     });
+     $('.right-img7').mouseleave(function(){
+         $('.img7-sub').css('display','none');
+      });
+      $('.right-img4').mouseover(function(){
+        $('.img4-sub').css('display','block');
+     });
+     $('.right-img4').mouseleave(function(){
+         $('.img4-sub').css('display','none');
+      });
+      $('.bg-black').mouseleave(function(){
+        $('.img-title').css('transform','scale(0.8)');
+     });
+});
