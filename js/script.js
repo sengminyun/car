412852af0f39dc3909dd73678412e385929bb781
sengminyun <64954108+sengminyun@users.noreply.github.com>
2020-07-05 19:10:45 +0900

    car

--- js/script.js
+++ js/script.js
@@ -107,54 +107,67 @@ $(document).ready(function() {
 
 $(function(){
     $('.right-img1').mouseover(function(){
+       $('.bg1').css('display','block');
        $('.img1-sub').css('display','block');
     });
     $('.right-img1').mouseleave(function(){
         $('.img1-sub').css('display','none');
+        $('.bg1').css('display','none');
      });
      $('.right-img2').mouseover(function(){
         $('.img2-sub').css('display','block');
+        $('.bg2').css('display','block');
      });
      $('.right-img2').mouseleave(function(){
          $('.img2-sub').css('display','none');
+         $('.bg2').css('display','none');
       });
       $('.right-img3').mouseover(function(){
         $('.img3-sub').css('display','block');
+        $('.bg3').css('display','block');
      });
      $('.right-img3').mouseleave(function(){
          $('.img3-sub').css('display','none');
-      });
-      $('.right-img8').mouseover(function(){
-        $('.img8-sub').css('display','block');
-     });
-     $('.right-img8').mouseleave(function(){
-         $('.img8-sub').css('display','none');
-      });
-      $('.right-img5').mouseover(function(){
-        $('.img5-sub').css('display','block');
-     });
-     $('.right-img5').mouseleave(function(){
-         $('.img5-sub').css('display','none');
-      });
-      $('.right-img6').mouseover(function(){
-        $('.img6-sub').css('display','block');
-     });
-     $('.right-img6').mouseleave(function(){
-         $('.img6-sub').css('display','none');
-      });
-      $('.right-img7').mouseover(function(){
-        $('.img7-sub').css('display','block');
-     });
-     $('.right-img7').mouseleave(function(){
-         $('.img7-sub').css('display','none');
+         $('.bg3').css('display','none');
       });
       $('.right-img4').mouseover(function(){
         $('.img4-sub').css('display','block');
+        $('.bg4').css('display','block');
      });
      $('.right-img4').mouseleave(function(){
          $('.img4-sub').css('display','none');
+         $('.bg4').css('display','none');
       });
-      $('.bg-black').mouseleave(function(){
-        $('.img-title').css('transform','scale(0.8)');
+      $('.right-img5').mouseover(function(){
+        $('.img5-sub').css('display','block');
+        $('.bg5').css('display','block');
      });
+     $('.right-img5').mouseleave(function(){
+         $('.img5-sub').css('display','none');
+         $('.bg5').css('display','none');
+      });
+      $('.right-img6').mouseover(function(){
+        $('.img6-sub').css('display','block');
+        $('.bg6').css('display','block');
+     });
+     $('.right-img6').mouseleave(function(){
+         $('.img6-sub').css('display','none');
+         $('.bg6').css('display','none');
+      });
+      $('.right-img7').mouseover(function(){
+        $('.img7-sub').css('display','block');
+        $('.bg7').css('display','block');
+     });
+     $('.right-img7').mouseleave(function(){
+         $('.img7-sub').css('display','none');
+         $('.bg7').css('display','none');
+      });
+      $('.right-img8').mouseover(function(){
+        $('.img8-sub').css('display','block');
+        $('.bg8').css('display','block');
+     });
+     $('.right-img8').mouseleave(function(){
+         $('.img8-sub').css('display','none');
+         $('.bg8').css('display','none');
+      });
 });
